c9da0321d5172dce56438c3d26be24119e3d3451
강승연 <syeon.dev@gmail.com>
2025-01-10 20:45:07 +0900

    [FEAT] 스탯어쩌구

--- Assets/Scripts/Data/GameData.cs
+++ Assets/Scripts/Data/GameData.cs
@@ -2,14 +2,5 @@
 
 public class GameData
 {
-    /// <summary>
-    /// 각 캐릭터 별 초기 스탯 지정
-    /// </summary>
-    public Dictionary<Character, CharacterStat> stats = new Dictionary<Character, CharacterStat>
-    {
-        { Character.Player, new CharacterStat()},
-        { Character.Boy, new CharacterStat()},
-        { Character.Girl, new CharacterStat()},
-        { Character.Furry, new CharacterStat()}
-    };
+    public CharacterStat stats = new CharacterStat();
 }
