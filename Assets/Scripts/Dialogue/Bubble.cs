8d5258b47217aec76ee0556c4d3b80f146d5ce5b
강승연 <syeon.dev@gmail.com>
2025-01-12 02:05:18 +0900

    [FEAT] 민트 기본 사운드

--- Assets/Scripts/Dialogue/Bubble.cs
+++ Assets/Scripts/Dialogue/Bubble.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using Yarn;
+using Yarn.Unity;
 
 public class Bubble : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private PortraitDisplay portrait;
+    [SerializeField] private TextMeshProUGUI text;
+    [SerializeField] private TextMeshProUGUI name;
+    [SerializeField] private DialogueRunner runner;
+    
     void Start()
     {
-        
+        runner.onNodeStart.AddListener(MintVoice);
     }
 
-    // Update is called once per frame
-    void Update()
+    void MintVoice(string comment)
     {
-        
+        if (name.text != "민트") return;
+
+        bool isIdle = false;
+        foreach (Image image in portrait.images)
+        {
+            if (image.sprite.name == "Mint_Idle") isIdle = true;
+        }
+
+        if (!isIdle) return;
+
+        if (text.text.Length > 2 && text.text[text.text.Length - 2] == '?')
+        {
+            SoundManager.Instance.PlaySFX("MintCurious");
+        }
+        else
+        {
+            System.Random random = new System.Random();
+            int randomNumber = random.Next(1, 2);
+            SoundManager.Instance.PlaySFX($"MintIdle{random}");
+        }
     }
 }
