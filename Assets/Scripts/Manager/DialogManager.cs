657b08f7a23563953f783691b2ad25520dd42840
Danny-IV <dh46656@gmail.com>
2025-01-11 15:54:57 +0900

    Feat: Ending

--- Assets/Scripts/Manager/DialogManager.cs
+++ Assets/Scripts/Manager/DialogManager.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using Yarn.Unity;
 
 public class DialogManager : Singleton<DialogManager>
 {
     public DialogueRunner dialogueRunner;
+    public LineView lineView;
     [SerializeField] private CoinDisplay coinDisplay;
 
     void Start()
@@ -12,9 +14,25 @@ public class DialogManager : Singleton<DialogManager>
         StartDialogue("크리스_민트_첫만남");
     }
 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // TODO: 선택지에서도 넘어가짐. 선택지에서는 선택지 눌러야 넘어가게 수정
+            lineView.OnContinueClicked();
+        }
+    }
+
     public void StartDialogue(string filename)
     {
         dialogueRunner.StartDialogue(filename);
+    }
+
+    public void EndDialogue()
+    {
+        Debug.Log("End of dialogue");
+        // TODO: 현재 상황에 따라 엔딩씬 띄우기
+        SceneManager.LoadScene("EndingReview");
     }
 
     [YarnCommand("ChangeStat")]
@@ -22,8 +40,8 @@ public class DialogManager : Singleton<DialogManager>
     {
         GameManager.Instance.data.GetStat(statName).ChangeStat(amount);
     }
-    
-    [YarnFunction ("TossCoin")]
+
+    [YarnFunction("TossCoin")]
     public static bool TossCoin(float successRate)
     {
         Debug.Log("Tossing a coin...");
@@ -34,7 +52,7 @@ public class DialogManager : Singleton<DialogManager>
         }
 
         // TODO: 코인 돌아가는 연출 추가하기, 이때 interaction은 끄기
-        
+
         return Random.value < successRate;
     }
 
