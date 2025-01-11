da65834254debc1f6d95fc10126e0fd00e89a545
강승연 <syeon.dev@gmail.com>
2025-01-11 15:56:50 +0900

    [ADD] 코인 테스트

---
+++ Assets/CoinTest.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinTest : MonoBehaviour
+{
+    void Start()
+    {
+        DialogManager.TossCoin2(1, "mov", 2, "mental");
+    }
+
+}
