6f661cfd448c8fde90a6528fd66832c16f532084
Danny-IV <dh46656@gmail.com>
2025-01-12 10:56:29 +0900

    Fix: 시작로직 제거거

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -4,15 +4,4 @@ using UnityEditor.SceneManagement;
 public class GameManager : Singleton<GameManager>
 {
     public GameData data = new GameData();
-
-    [InitializeOnLoad]
-    public class EditorStartInit
-    {
-        static EditorStartInit()
-        {
-            var pathOfFirstScene = EditorBuildSettings.scenes[0].path;
-            var sceneAsset = AssetDatabase.LoadAssetAtPath<SceneAsset>(pathOfFirstScene);
-            EditorSceneManager.playModeStartScene = sceneAsset;
-        }
-    }
 }
