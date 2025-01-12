53826a98f0959742fd5b61d70feb266596266bb6
Danny-IV <dh46656@gmail.com>
2025-01-12 11:10:24 +0900

    Chore: reset testing

--- Assets/Scripts/Manager/DialogManager.cs
+++ Assets/Scripts/Manager/DialogManager.cs
@@ -42,17 +42,18 @@ public class DialogManager : Singleton<DialogManager>
 
     void Init()
     {
-        List<string> earlyDialog = new List<string>() { "크리스_은채_첫번째", "크리스_민트_첫번째", "은채_민트_첫번째" };
-        List<string> midDialog = new List<string>() { "크리스_은채_두번째", "크리스_은채_세번째",
-            "크리스_민트_두번째", "크리스_민트_세번째", "은채_민트_두번째", "은채_민트_세번째"}; // 중간거는 6개인데, 3~4개정도만 내보내기
-        List<string> lastDialog = new List<string>() { "크리스_은채_네번째", "크리스_민트_네번째", "은채_민트_네번째" };
+        List<string> earlyDialog = new List<string>() { "크리스_은채_첫번째" };
+        // List<string> earlyDialog = new List<string>() { "크리스_은채_첫번째", "크리스_민트_첫번째", "은채_민트_첫번째" };
+        // List<string> midDialog = new List<string>() { "크리스_은채_두번째", "크리스_은채_세번째",
+        //     "크리스_민트_두번째", "크리스_민트_세번째", "은채_민트_두번째", "은채_민트_세번째"}; // 중간거는 6개인데, 3~4개정도만 내보내기
+        // List<string> lastDialog = new List<string>() { "크리스_은채_네번째", "크리스_민트_네번째", "은채_민트_네번째" };
         dialogList = new List<string>();
         dialogList.AddRange(Shuffle(earlyDialog));
-        List<string> temp = Shuffle(midDialog);
-        dialogList.Add(temp[0]);
-        dialogList.Add(temp[1]);
-        dialogList.Add(temp[2]);
-        dialogList.AddRange(Shuffle(lastDialog));
+        // List<string> temp = Shuffle(midDialog);
+        // dialogList.Add(temp[0]);
+        // dialogList.Add(temp[1]);
+        // dialogList.Add(temp[2]);
+        // dialogList.AddRange(Shuffle(lastDialog));
     }
 
     public static List<string> Shuffle(List<string> values)
@@ -78,6 +79,7 @@ public class DialogManager : Singleton<DialogManager>
         {
             SceneManager.LoadScene("EndingReview");
             GameResettor.needReset = true;
+            hasSeenEnding = false;
             return;
         }
 
