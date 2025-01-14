51dafbedb0d4cf7e586134040e281c140fe099a9
sh1257 <ligerhearts@gmail.com>
2025-01-14 10:42:45 +0900

    [FIX] Initial Stat Update

--- Assets/Scripts/Data/CharacterStat.cs
+++ Assets/Scripts/Data/CharacterStat.cs
@@ -3,13 +3,13 @@
 public class CharacterStat
 {
     //스탯 초기값 설정
-    public Stat mov = new Stat(65);
-    public Stat charm = new Stat(65);
-    public Stat mental = new Stat(65);
+    public Stat mov = new Stat(60);
+    public Stat charm = new Stat(60);
+    public Stat mental = new Stat(60);
     public Stat lvChris = new Stat(50);
     public Stat lvEun = new Stat(50);
     public Stat lvMint = new Stat(50);
-    public Stat Chris_Eun = new Stat(70);
-    public Stat Eun_Mint = new Stat(70);
-    public Stat Mint_Chris = new Stat(70);
+    public Stat Chris_Eun = new Stat(50);
+    public Stat Eun_Mint = new Stat(50);
+    public Stat Mint_Chris = new Stat(50);
 }
